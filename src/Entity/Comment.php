6c644c72fa0f068d9d538e72c739ebb2616eb1ed
LucArronne <hajarjh@yahoo.fr>
2024-10-17 18:52:41 +0300

    update  fields validation

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
@@ -15,9 +16,11 @@ class Comment
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(message: "The pseudo is required")]
     private ?string $pseudo = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: "The text is required")]
     private ?string $text = null;
 
     #[ORM\Column(nullable: true)]
