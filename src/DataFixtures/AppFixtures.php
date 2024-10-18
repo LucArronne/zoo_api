bf14ca67b6a614869c4b1a6c31c5e070be8fc06b
LucArronne <hajarjh@yahoo.fr>
2024-10-18 19:53:54 +0300

    update data generator

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Comment;
+use App\Entity\Role;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -18,9 +19,29 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
+        $roleAdmin = new Role();
+        $roleAdmin->setName("Administateur");
+        $roleAdmin->setValue("ROLE_ADMIN");
+
+        $manager->persist($roleAdmin);
+
+
+        $roleEmployee = new Role();
+        $roleEmployee->setName("Employé");
+        $roleEmployee->setValue("ROLE_EMPLOYEE");
+
+        $manager->persist($roleEmployee);
+
+
+        $roleVeternaire = new Role();
+        $roleVeternaire->setName("Vétérnaire");
+        $roleVeternaire->setValue("ROLE_VETERNARY");
+
+        $manager->persist($roleVeternaire);
+
         $userAdmin = new User();
         $userAdmin->setEmail("admin@zoo.org");
-        $userAdmin->setRole("ROLE_ADMIN");
+        $userAdmin->setRole($roleAdmin);
         $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
 
         $manager->persist($userAdmin);
@@ -28,11 +49,12 @@ class AppFixtures extends Fixture
 
         $userEmployee = new User();
         $userEmployee->setEmail("demo@zoo.org");
-        $userEmployee->setRole("ROLE_EMPLOYEE");
+        $userEmployee->setRole($roleEmployee);
         $userEmployee->setName("Demo");
         $userEmployee->setPassword($this->userPasswordHasher->hashPassword($userEmployee, "password"));
 
         $manager->persist($userEmployee);
+
 
 
         for ($i = 0; $i < 10; $i++) {
