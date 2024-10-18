6ad74de96ad67137830b64f77724831e5b4ce63c
LucArronne <hajarjh@yahoo.fr>
2024-10-18 21:22:02 +0300

    update role/user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,8 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
@@ -14,22 +16,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['getUsers'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['getUsers'])]
+    #[Assert\NotBlank(message: "The email is required")]
+    #[Assert\Email(message:"The email is invalid")]
     private ?string $email = null;
-
-    #[ORM\Column(length: 30)]
-    private ?string $role = null;
 
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message: "The password is required")]
     private ?string $password = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(['getUsers'])]
     private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'users')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['getUsers'])]
+    private ?Role $role = null;
 
     public function getId(): ?int
     {
@@ -70,15 +80,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
-        return [$this->role];
+        return [$this->role->getValue()];
     }
 
-    public function getRole(): ?string
+    public function getRole(): ?Role
     {
         return $this->role;
     }
 
-    public function setRole(string $role): static
+    public function setRole(?Role $role): static
     {
         $this->role = $role;
 
