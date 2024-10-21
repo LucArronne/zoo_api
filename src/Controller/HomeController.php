805efa83a798c9b2f97c644d7bd13324cc204d5e
LucArronne <hajarjh@yahoo.fr>
2024-10-21 10:34:02 +0300

    implement some endpoints on Home Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Repository\AnimalRepository;
 use App\Repository\CommentRepository;
+use App\Repository\HabitatRepository;
 use App\Repository\ServiceRepository;
 use App\Utils\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,7 +14,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -21,6 +21,118 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 #[Route(path: '/home')]
 class HomeController extends AbstractController
 {
+
+    #[Route(path: '/habitats', name: 'habitats', methods: ['GET'])]
+    public function getHabitas(
+        HabitatRepository $habitatRepository,
+        FileUploader $uploader,
+        SerializerInterface $serializer,
+    ): JsonResponse {
+
+        $habitatList = $habitatRepository->findAll();
+
+        $result = [];
+        foreach ($habitatList as $habitat) {
+            $images = [];
+            foreach ($habitat->getImages() as $image) {
+                $images[] = [
+                    'id' => $image->getId(),
+                    'path' => $uploader->getFilePublicUrl($image->getPath())
+                ];
+            }
+            $animals = [];
+            foreach ($habitat->getAnimals() as $animal) {
+                $animals[] = [
+                    'id' => $animal->getId(),
+                    'name' => $animal->getName(),
+                    'race' => $animal->getRace(),
+                    'images' => $animal->getImages()->map(function ($image) use ($uploader) {
+                        return [
+                            'id' => $image->getId(),
+                            'path' => $uploader->getFilePublicUrl($image->getPath()),
+                        ];
+                    }),
+                ];
+            }
+            $result[] = [
+                'id' => $habitat->getId(),
+                'name' => $habitat->getName(),
+                'descrption' => $habitat->getDescription(),
+                'images' => $images,
+                'animals' => $animals,
+            ];
+        }
+
+        return new JsonResponse(
+            $serializer->serialize($result, 'json'),
+            Response::HTTP_OK,
+            [],
+            true,
+        );
+    }
+
+    #[Route('/services', name: 'services', methods: ['GET'])]
+    public function getServices(
+        ServiceRepository $serviceRepository,
+        SerializerInterface $serializer,
+        FileUploader $uploader,
+    ): JsonResponse {
+
+        $serviceList = $serviceRepository->findAll();
+
+        $result = [];
+
+        foreach ($serviceList as $service) {
+            $result[] = [
+                'id' => $service->getId(),
+                'name' => $service->getName(),
+                'description' => $service->getDescription(),
+                'image' => $service->getImage() === null
+                    ? null
+                    : $uploader->getFilePublicUrl($service->getImage()),
+            ];
+        }
+
+        return new JsonResponse(
+            $serializer->serialize($result, 'json'),
+            Response::HTTP_OK,
+            [],
+            true,
+        );
+    }
+
+    #[Route('/animals', name: 'animals', methods: ['GET'])]
+    public function getAnimals(
+        AnimalRepository $animalRepository,
+        SerializerInterface $serializer,
+        FileUploader $uploader,
+    ): JsonResponse {
+        $animalList = $animalRepository->findAll();
+
+        $result = [];
+
+        foreach ($animalList as $animal) {
+            $result[] = [
+                'id' => $animal->getId(),
+                'name' => $animal->getName(),
+                'race' => $animal->getRace(),
+                'images' => $animal->getImages()->map(function ($image) use ($uploader) {
+                    return [
+                        'id' => $image->getId(),
+                        'path' => $uploader->getFilePublicUrl($image->getPath()),
+                    ];
+                }),
+            ];
+        }
+
+        return new JsonResponse(
+            $serializer->serialize($result, 'json'),
+            Response::HTTP_OK,
+            [],
+            true,
+        );
+    }
+
     #[Route('/comments', name: 'approuved-comments', methods: ['GET'])]
     public function getApprouvedComments(CommentRepository $commentRepository, SerializerInterface $serializer): JsonResponse
     {
@@ -53,69 +165,5 @@ class HomeController extends AbstractController
         $jsonComment = $serializer->serialize($comment, 'json');
 
         return new JsonResponse($jsonComment, Response::HTTP_CREATED, [], true);
-    }
-
-    #[Route('/services', name: 'services', methods: ['GET'])]
-    public function getServices(
-        ServiceRepository $serviceRepository,
-        SerializerInterface $serializer,
-        FileUploader $uploader,
-        UrlGeneratorInterface $urlGenerator,
-    ): JsonResponse {
-        $serviceList = $serviceRepository->findAll();
-
-        $services = [];
-
-        foreach ($serviceList as $service) {
-            $services[] = [
-                'id' => $service->getId(),
-                'name' => $service->getName(),
-                'description' => $service->getDescription(),
-                'image' => $service->getImage() === null
-                    ? null
-                    : $uploader->getFilePublicUrl($urlGenerator, $service->getImage()),
-            ];
-        }
-
-        $jsonServiceList = $serializer->serialize($services, 'json');
-
-        return new JsonResponse(
-            $jsonServiceList,
-            Response::HTTP_OK,
-            [],
-            true,
-        );
-    }
-
-    #[Route('/animals', name: 'animals', methods: ['GET'])]
-    public function getAnimals(
-        AnimalRepository $animalRepository,
-        SerializerInterface $serializer,
-        FileUploader $uploader,
-        UrlGeneratorInterface $urlGenerator,
-    ): JsonResponse {
-        $animalList = $animalRepository->findAll();
-
-        $animals = [];
-
-        foreach ($animalList as $animal) {
-            $animals[] = [
-                'id' => $animal->getId(),
-                'name' => $animal->getName(),
-                'race' => $animal->getRace(),
-                'images' => $animal->getImages()->map(function ($filename) use ($uploader, $urlGenerator) {
-                    return $uploader->getFilePublicUrl($urlGenerator, $filename->getPath());
-                }),
-            ];
-        }
-
-        $jsonAnimalList = $serializer->serialize($animals, 'json', ['groups' => "getAnimals"]);
-
-        return new JsonResponse(
-            $jsonAnimalList,
-            Response::HTTP_OK,
-            [],
-            true,
-        );
     }
 }
