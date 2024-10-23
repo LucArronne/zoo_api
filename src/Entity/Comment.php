41d4833ea0b00fb802e082521abd4c51d83668e3
LucArronne <hajarjh@yahoo.fr>
2024-10-23 18:38:24 +0300

    refactor comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -75,12 +75,12 @@ class Comment
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getCreatedAt(): ?DateTimeImmutable
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    public function setCreatedAt(DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
 
