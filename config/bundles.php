9e21e1f109c7825a55e04407f8ecb1345df84a1f
LucArronne <hajarjh@yahoo.fr>
2024-12-02 15:49:54 +0300

    add doctrine/mongodb-odm-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
